541313c436feda187c8ab2504076c27952732c48
Sivabudh Umpudh <sivabudh.u@codium.co>
2018-12-08 00:47:51 +0700

    First image display

--- shironeko/views.py
+++ shironeko/views.py
@@ -1,25 +1,42 @@
-import logging, datetime, pytz, locale
+import base64
+import logging
 
-from rest_framework import viewsets
-from num2words import num2words
-from rest_framework.decorators import list_route
-from rest_framework.pagination import PageNumberPagination
 from django.http import HttpResponse
-from rest_framework.views import APIView
+from django.template import loader
+from django.template.loader import get_template
 from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
 from weasyprint import HTML
 
-from django.template.loader import render_to_string
-
 log = logging.getLogger('hero_track')
+
 
 class PreviewSalaryTemplates(APIView):
     permission_classes = (AllowAny,)
 
     def get(self, request):
-        html_string = render_to_string(template_name='salary_guarantee_template.html')
-        response = HttpResponse(content_type='application/pdf;')
-        response['Content-Disposition'] = 'inline; filename=salary_guarantee_form.pdf'
-        HTML(string=html_string, base_url=request.build_absolute_uri()).write_pdf(response)
-        print(response)
-        return response
+        context = {}
+
+        filename = "/Users/pac/Documents/Code/testpdf/static/ductasia.jpg"
+        with open(filename, "rb") as imagefile:
+            encoded_string = base64.b64encode(imagefile.read())
+            context["img"] = encoded_string
+        #
+        # html_template = get_template('salary_guarantee_template.html')
+        # html = html_template.render(context).encode(encoding="UTF-8")
+        # uri = request.build_absolute_uri()
+        #
+        # response = HttpResponse(content_type='application/pdf;')
+        # response['Content-Disposition'] = 'inline; filename=salary_guarantee_form.pdf'
+        #
+        # HTML(
+        #     string=html,
+        #     base_url=uri,
+        #     encoding="UTF-8"
+        # ).write_pdf(response)
+        #
+        # return response
+
+        template = loader.get_template('salary_guarantee_template.html')
+        response = template.render(context, request)
+        return HttpResponse(response)

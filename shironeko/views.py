31c8426e214c9f6b31018f690c78f210660b4ea3
Sivabudh Umpudh <sivabudh.u@codium.co>
2018-12-08 01:12:36 +0700

    Reduced code a bit more

--- shironeko/views.py
+++ shironeko/views.py
@@ -15,23 +15,18 @@ class PreviewSalaryTemplates(APIView):
     permission_classes = (AllowAny,)
 
     def get(self, request):
-        context = {}
+        html_template = get_template('salary_guarantee_template.html')
+        html = html_template.render().encode(encoding="UTF-8")
 
-        if True:
-            html_template = get_template('salary_guarantee_template.html')
-            html = html_template.render(context).encode(encoding="UTF-8")
-
+        if False:
             uri = request.build_absolute_uri()
 
-            pdf_file = HTML(
+            pdf = HTML(
                 string=html,
                 base_url=uri,
             ).write_pdf()
 
-            response = HttpResponse(pdf_file, content_type='application/pdf')
+            response = HttpResponse(pdf, content_type='application/pdf')
             return response
         else:
-            template = loader.get_template('salary_guarantee_template.html')
-            response = template.render(context, request)
-
-            return HttpResponse(response)
+            return HttpResponse(html)

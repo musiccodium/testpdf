4138f0d2667f847289fcaf72fbb690adb814e0b7
Sivabudh Umpudh <sivabudh.u@codium.co>
2018-12-08 01:09:50 +0700

    Refactored code a bit

--- shironeko/views.py
+++ shironeko/views.py
@@ -17,26 +17,21 @@ class PreviewSalaryTemplates(APIView):
     def get(self, request):
         context = {}
 
-        filename = "/Users/pac/Documents/Code/testpdf/static/ductasia.jpg"
-        with open(filename, "rb") as imagefile:
-            encoded_string = base64.b64encode(imagefile.read())
-            context["img"] = encoded_string
-        #
-        # html_template = get_template('salary_guarantee_template.html')
-        # html = html_template.render(context).encode(encoding="UTF-8")
-        # uri = request.build_absolute_uri()
-        #
-        # response = HttpResponse(content_type='application/pdf;')
-        # response['Content-Disposition'] = 'inline; filename=salary_guarantee_form.pdf'
-        #
-        # HTML(
-        #     string=html,
-        #     base_url=uri,
-        #     encoding="UTF-8"
-        # ).write_pdf(response)
-        #
-        # return response
+        if True:
+            html_template = get_template('salary_guarantee_template.html')
+            html = html_template.render(context).encode(encoding="UTF-8")
 
-        template = loader.get_template('salary_guarantee_template.html')
-        response = template.render(context, request)
-        return HttpResponse(response)
+            uri = request.build_absolute_uri()
+
+            pdf_file = HTML(
+                string=html,
+                base_url=uri,
+            ).write_pdf()
+
+            response = HttpResponse(pdf_file, content_type='application/pdf')
+            return response
+        else:
+            template = loader.get_template('salary_guarantee_template.html')
+            response = template.render(context, request)
+
+            return HttpResponse(response)
